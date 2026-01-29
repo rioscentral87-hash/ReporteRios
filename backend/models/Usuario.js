de1a2c74661f74f2c8915dc45b4974564113b27e
rioscentral87-hash <rios.central87@gmail.com>
2026-01-29 16:55:49 -0600

    Update Usuario.js

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -1,60 +1,21 @@
-/*const mongoose = require("mongoose");
 
-const UsuarioSchema = new mongoose.Schema({
-  nombre: String,
-  password: String, // luego se puede encriptar
-  rol: {
-    type: String,
-    enum: ["SUPERVISOR", "PASTOR"]
-  },
-  sector: Number // solo para supervisor
-});
-
-module.exports = mongoose.model("Usuario", UsuarioSchema);*/
-
-/*const mongoose = require("mongoose");
-
-const UsuarioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  rol: { type: String, enum: ["PASTOR", "SUPERVISOR"], required: true },
-  sector: { type: Number }
-});
-
-module.exports = mongoose.model("Usuario", UsuarioSchema);*/
-
-//const mongoose = require("mongoose");
-
-/*const UsuarioSchema = new mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  rol: {
-    type: String,
-    enum: ["PASTOR", "SUPERVISOR"],
-    required: true
-  },
-  sector: {
-    type: Number
-  }
-});
-
-module.exports = mongoose.model("Usuario", UsuarioSchema);
-*/
 
 const mongoose = require("mongoose");
 
 const UsuarioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  password: { type: String, required: true },
-  rol: { type: String, required: true },
-  sector: { type: Number }
+  nombre: String,
+  password: String,
+  rol: {
+    type: String,
+    enum: ["PASTOR", "SUPERVISOR","COMITE"]
+  },
+  sector: Number,
+  tipoSupervisor: {
+    type: String,
+    enum: ["Adulto", "Juvenil", "Infantil"]
+  }
 });
+
 
 module.exports = mongoose.model("Usuario", UsuarioSchema);
 
