492b295e9c01d75f437fa342ef265f52704c8696
rioscentral87-hash <rios.central87@gmail.com>
2026-04-21 18:14:12 -0600

    Update reportes.routes.js

--- backend/routes/reportes.routes.js
+++ backend/routes/reportes.routes.js
@@ -190,7 +190,7 @@ router.get("/pendientes-comite", async (req, res) => {
 
 router.get("/pendientes-comite-semana", async (req, res) => {
   try {
-    const semanaActual = getSemanaActual()-1; // helper
+    const semanaActual = getSemanaActual(); // helper
     const semanaRevision = semanaActual - 1;
 
     const reportes = await Reporte.find({
